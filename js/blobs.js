bc8d9dd91ff98a1a78a3e8a85ecdd9442c6a7f60
Sam <sam.shaw@usu.edu>
2024-03-06 13:55:36 -0700

    code cleanup

--- js/blobs.js
+++ js/blobs.js
@@ -13,13 +13,8 @@ function movesBlob() {
     sblob.style.top = getRandomInt(20, 30) + 'vw';
     sblob.style.left = getRandomInt(48, 65) + 'vw';
 
-    // Add the transform so it can be centered in the viewport
-    // blob.style.transform = 'translate(-50%,-50%)';
-
     // Call moveBlob again after a certain delay
-
     var stime = getRandomInt(2000, 5000);
-
     setTimeout(movesBlob, stime);
 }
 
@@ -29,13 +24,8 @@ function movelBlob() {
     lblob.style.top = getRandomInt(19, 30) + 'vw';
     lblob.style.left = getRandomInt(49, 65) + 'vw';
 
-    // Add the transform so it can be centered in the viewport
-    // blob.style.transform = 'translate(-50%,-50%)';
-
     // Call moveBlob again after a certain delay
-
     var ltime = getRandomInt(2000, 5000);
-
     setTimeout(movelBlob, ltime);
 }
 
